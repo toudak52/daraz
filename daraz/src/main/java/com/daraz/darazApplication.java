2ce761dc73edc886f9dc42766b257a3c373fae31
DESKTOP-A2QDNCN\Kimsiyeong <toudak52@naver.com>
2023-03-29 11:19:18 +0900

    project set

---
+++ daraz/src/main/java/com/daraz/darazApplication.java
@@ -0,0 +1,12 @@
+package com.daraz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class darazApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.daraz.darazApplication.class, args);
+    }
+
+}
